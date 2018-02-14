165d6a2b476a7bf39f96e331365263681651c206
vpyadav8961 <vpyadav8961@gmail.com>
2018-02-15 00:12:32 +0530

    Basic format of choose file added

---
+++ FileUpload.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>Upload File Onto Server</title>
+	</head>
+	<body>
+		<form action="upload.php" method = "post">
+			Upload you file::<input type="file" name="file">
+			<input type="submit" name="submit" value = "submit">
+		</form>
+	</body>
+</html>
